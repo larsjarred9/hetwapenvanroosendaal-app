1e9e95146434748c18b32892b8ae5055ee499320
Danny de Jonge <dannydejonge1796@gmail.com>
2023-04-28 10:25:41 +0200

    getting beers from database

--- app/src/main/java/com/example/hetwapenvanroosendaal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hetwapenvanroosendaal/ui/home/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.example.hetwapenvanroosendaal.ui.home
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.hetwapenvanroosendaal.databinding.FragmentHomeBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
@@ -21,6 +25,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val db = Firebase.firestore
+
+        db.collection("beers")
+            .limit(4)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(ContentValues.TAG, "${document.id} => ${document.data["name"]}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(ContentValues.TAG, "Error getting documents.", exception)
+            }
 
 
         return binding.root
--- app/src/main/java/com/example/hetwapenvanroosendaal/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/hetwapenvanroosendaal/ui/home/HomeViewModel.kt
@@ -1,9 +1,8 @@
 package com.example.hetwapenvanroosendaal.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+
 
 }
