ead8fb0afa88e1704f40d275f89e0340fe391ead
Danny de Jonge <dannydejonge1796@gmail.com>
2023-05-16 12:52:00 +0200

    loading beer card web view and opening links inside of browser

--- app/src/main/java/com/example/hetwapenvanroosendaal/ui/beer/BeerFragment.kt
+++ app/src/main/java/com/example/hetwapenvanroosendaal/ui/beer/BeerFragment.kt
@@ -1,10 +1,18 @@
 package com.example.hetwapenvanroosendaal.ui.beer
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import com.example.hetwapenvanroosendaal.R
 import com.example.hetwapenvanroosendaal.databinding.FragmentBeerBinding
 
 class BeerFragment : Fragment() {
@@ -15,6 +23,7 @@ class BeerFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,7 +31,28 @@ class BeerFragment : Fragment() {
     ): View {
         _binding = FragmentBeerBinding.inflate(inflater, container, false)
 
+        val webView = binding.root.findViewById<WebView>(R.id.beerWebView)
 
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                val url = request.url?.toString()
+                try {
+                    url?.let {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                        startActivity(intent)
+                    }
+                } catch (e: Exception) {
+                    Log.e("WebView", "Error opening URL: $url", e)
+                }
+                return true // Indicate that the URL loading is handled externally
+            }
+        }
+
+        // Enable JavaScript (optional)
+        webView.settings.javaScriptEnabled = true
+
+        // Load the website
+        webView.loadUrl("https://bouwmarkt.speetjens.net/bier.html")
 
         return binding.root
     }
