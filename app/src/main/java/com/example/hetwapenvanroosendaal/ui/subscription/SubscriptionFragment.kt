6905f486a629e6017e6b270c42974b7445a0bcb9
Danny de Jonge <dannydejonge1796@gmail.com>
2023-05-17 13:38:20 +0200

    added images to subscription page and added navigation action

---
+++ app/src/main/java/com/example/hetwapenvanroosendaal/ui/subscription/SubscriptionFragment.kt
@@ -0,0 +1,37 @@
+package com.example.hetwapenvanroosendaal.ui.subscription
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hetwapenvanroosendaal.R
+import com.example.hetwapenvanroosendaal.databinding.FragmentSubscriptionBinding
+
+class SubscriptionFragment : Fragment() {
+
+    private var _binding: FragmentSubscriptionBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSubscriptionBinding.inflate(inflater, container, false)
+
+        //Set the header texts of the page
+        _binding!!.homeHeader.pageTitle.text = requireContext().getString(R.string.subscription)
+        _binding!!.homeHeader.pageDescription.text = requireContext().getString(R.string.subscription_description)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
